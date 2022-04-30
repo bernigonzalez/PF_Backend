ebaf7cfbdb8a9b18632b545280d2bd0047e7f636
arcefranco <arcefranco29@gmail.com>
2022-04-30 15:22:33 -0300

    ruta para actualizar img creada

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -276,7 +276,62 @@ userRouter.put("/update", [
     return next({});
   }
 });
+////////////UPDATE IMG
+userRouter.put("/updateImg",/*  [
+  check('id', 'Incluya un "id" valido').isInt({ min: 1 }),
+  check('nombre', 'Incluya un "nombre" valido').isString().trim().not().isEmpty(),
+  check('usuario', 'Incluya un "usuario" valido').isString().trim().not().isEmpty(),
+  check('contrasena', 'Incluya una contraseña válida').isString().trim().not().isEmpty(),
+  check('email', 'Incluya un email válido').isEmail().exists(),
+  check('pais', 'Incluya un país válido').isString().trim().not().isEmpty(),
+  check('provincia', 'Incluya una provincia válida').isString().trim().not().isEmpty(),
+  check('direccion', 'Incluya una direccion válida').isString().trim().not().isEmpty(),
+  check('telefono', 'Incluya un telefono válido').isString().isLength({ min: 8 }),
+], authentication, */ async (req, res, next) => {
+  // Validaciones de express-validator
+ /*  const errors = validationResult(req);
 
+  if (!errors.isEmpty()) {
+    return next({ status: 400, errors });
+  } */
+
+  // Si no hay errores, continúo
+  const {
+    id,
+    img,
+  } = req.body;
+
+  if (!id) return next({ status: 400, message: "El id es Requerido" });
+/*   let avata = gravatar.url(email, {
+    s: "200", //size
+    r: "pg", //rate
+    d: "mm",
+  }); */
+  try {
+/*     let password = await bcrypt.hash(contrasena, 10); */
+    const UserUpdate = await Usuario.update(
+      {
+        avatar: img,
+
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    if (UserUpdate)
+      return res
+        .status(200)
+        .json({ message: "Los Datos fueron Actualizados" });
+
+    return res.status(203).json({ message: "Algo Sucedio" });
+  } catch (error) {
+    return next({});
+  }
+});
+
+//////////////////
 
 // @route PUT user/block/:userId
 // @desc Bloquear un usuario
