5299247fd727b7c20af678b814cb3cb2416b964c
Bernardo Gonzalez <60318571+bernigonzalez@users.noreply.github.com>
2022-05-18 16:10:30 -0300

    adding pre-loaded data

--- src/data/pedidos.db.js
+++ src/data/pedidos.db.js
@@ -1,43 +1,9 @@
 module.exports = [
-   {
-      "usuarioId": 1,
-      "pedidos": [
-         { "productoId": 1, "cantidad": 4 },
-         { "productoId": 2, "cantidad": 6 },
-         { "productoId": 3, "cantidad": 10 },
-         { "productoId": 8, "cantidad": 3 }
-      ]
-   },
-   {
-      "usuarioId": 2,
-      "pedidos": [
-         { "productoId": 6, "cantidad": 2 },
-         { "productoId": 10, "cantidad": 1 },
-         { "productoId": 26, "cantidad": 1 },
-      ]
-   },
+   
    {
       "usuarioId": 3,
       "pedidos": [
-         { "productoId": 1, "cantidad": 2 },
-         { "productoId": 5, "cantidad": 3 },
-         { "productoId": 9, "cantidad": 1 },
-      ]
-   },
-   {
-      "usuarioId": 1,
-      "pedidos": [
-         { "productoId": 6, "cantidad": 2 },
-         { "productoId": 26, "cantidad": 1 },
-      ]
-   },
-   {
-      "usuarioId": 3,
-      "pedidos": [
-         { "productoId": 20, "cantidad": 1 },
-         { "productoId": 21, "cantidad": 1 },
-         { "productoId": 31, "cantidad": 1 },
-         { "productoId": 32, "cantidad": 1 }
+         { "productoId": 20, "cantidad": 1 }
       ]
    },
 ];
